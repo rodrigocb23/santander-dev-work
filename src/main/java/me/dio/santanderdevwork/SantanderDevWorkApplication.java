4112ac3415b522a483622e0200a382616482a4d0
rodrigocb23 <rodrigocb2012@gmail.com>
2024-03-27 20:12:39 -0300

    Initial commit

---
+++ src/main/java/me/dio/santanderdevwork/SantanderDevWorkApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santanderdevwork/SantanderDevWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
